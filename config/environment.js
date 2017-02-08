f7ebf1aecc02e5916f941d39bc629a246330c5ed
Todd Gandee <tgandee@bignerdranch.com>
2017-02-08 13:56:32 -0500

    add config for engine

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,11 @@
 /*jshint node:true*/
 'use strict';
 
-module.exports = function(/* environment, appConfig */) {
-  return { };
+module.exports = function(environment) {
+  const ENV = {
+    modulePrefix: 'external-admin',
+    environment: environment
+  }
+
+  return ENV;
 };

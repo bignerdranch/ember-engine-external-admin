a28bd4d1e0f1b01e7c9f557c8bab4c00bc64c149
Todd Gandee <tgandee@bignerdranch.com>
2017-02-09 13:56:21 -0500

    update returning an array of posts

--- addon/routes/posts.js
+++ addon/routes/posts.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   dataStore: Ember.inject.service(),
   model(){
-    return this.get('dataStore').createRecord("post", {title: "My Post", desc: "My description of post"});
+    return [this.get('dataStore').createRecord("post", {title: "My Post", desc: "My description of post"})];
   }
 });

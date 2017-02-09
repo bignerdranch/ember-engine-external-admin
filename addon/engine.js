bb34a358cfa0fd750ccd05130e5ea4f87e827723
Todd Gandee <tgandee@bignerdranch.com>
2017-02-09 10:08:30 -0500

    add external links

--- addon/engine.js
+++ addon/engine.js
@@ -6,7 +6,15 @@ import config from './config/environment';
 const { modulePrefix } = config;
 const Eng = Engine.extend({
   modulePrefix,
-  Resolver
+  Resolver,
+  dependencies: {
+    externalRoutes: [
+      'home',
+      'blog',
+      'faqs'
+    ]
+  }
+
 });
 
 loadInitializers(Eng, modulePrefix);
